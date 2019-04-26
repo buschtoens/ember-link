53edfa792389291e0a60fbcc9050fdf0688eedb7
Jan Buschtöns <jan@buschtoens.me>
2019-04-26 14:59:31 +0200

    chore: clean up `.ember-cli`

--- .ember-cli.js
+++ .ember-cli.js
@@ -9,5 +9,7 @@ module.exports = {
 
     Setting `disableAnalytics` to true will prevent any data from being sent.
   */
-  disableAnalytics: false
+  disableAnalytics: false,
+
+  usePods: true
 };
