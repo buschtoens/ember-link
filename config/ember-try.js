2b799d87779fe93445c216fcde189f7849ec25e4
Jan Buschtöns <jan@buschtoens.me>
2020-01-24 14:01:44 +0100

    chore: update `ember-try` config

--- config/ember-try.js
+++ config/ember-try.js
@@ -28,6 +28,30 @@ module.exports = function() {
           }
         },
         {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.12.0'
+            }
+          }
+        },
+        {
+          name: 'ember-3.15', // First Octane release
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.15.0'
+            }
+          }
+        },
+        {
           name: 'ember-release',
           npm: {
             devDependencies: {
