b313055aad27592ccdd6d285600389a55d0be83c
Jan Buschtöns <jan@buschtoens.me>
2020-01-24 15:35:55 +0100

    chore(deps-dev): add `ember-array-helper`

--- tests/dummy/app/helpers/array.ts
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-// Not natively available in Ember 3.4 and below
-export function array<T extends unknown[] = unknown[]>(params: T): T {
-  return params;
-}
-
-export default helper(array);
