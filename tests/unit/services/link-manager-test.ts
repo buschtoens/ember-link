745f35ec3da4d94abc40a2fb49e9ba68175f1f4f
Theodor Storm <tstormk@gmail.com>
2020-01-28 16:57:33 +0100

    Add test for currentTransitionStack in LinkManagerService

--- tests/unit/services/link-manager-test.ts
+++ tests/unit/services/link-manager-test.ts
@@ -1,12 +1,89 @@
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
+import Evented from '@ember/object/evented';
+import Transition from '@ember/routing/-private/transition';
+import Service from '@ember/service';
+
+import LinkManagerService from 'dummy/services/link-manager';
+
+function makeDummyTransition(): Transition {
+  return {
+    from: null,
+    to: {
+      child: null,
+      localName: 'dummy',
+      name: 'dummy',
+      paramNames: [],
+      params: {},
+      parent: null,
+      queryParams: {},
+      find() {
+        return undefined;
+      }
+    },
+    abort() {
+      return this;
+    },
+    retry() {
+      return this;
+    }
+  };
+}
+
 module('Unit | Service | link-manager', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const service = this.owner.lookup('service:link-manager');
-    assert.ok(service);
+  test('it manages currentTransitionStack correctly', function(assert) {
+    this.owner.register(
+      'service:router',
+      class MockRouterService extends Service.extend(Evented) {}
+    );
+
+    const linkManager = this.owner.lookup(
+      'service:link-manager'
+    ) as LinkManagerService;
+    const router = this.owner.lookup('service:router');
+
+    const firstTransition = makeDummyTransition();
+    const secondTransition = makeDummyTransition();
+    const updateAssertionMessage =
+      'transition stack is updated on routeWillChange with passed-in transition object';
+
+    router.trigger('routeWillChange', firstTransition);
+
+    assert.strictEqual(
+      linkManager.currentTransitionStack.length,
+      1,
+      updateAssertionMessage
+    );
+
+    assert.strictEqual(
+      linkManager.currentTransitionStack[0],
+      firstTransition,
+      updateAssertionMessage
+    );
+
+    router.trigger('routeWillChange', secondTransition);
+
+    assert.strictEqual(
+      linkManager.currentTransitionStack.length,
+      2,
+      updateAssertionMessage
+    );
+
+    assert.strictEqual(
+      linkManager.currentTransitionStack[1],
+      secondTransition,
+      updateAssertionMessage
+    );
+
+    router.trigger('routeDidChange');
+
+    assert.strictEqual(
+      linkManager.currentTransitionStack.length,
+      0,
+      'transition stack is cleared on routeDidChange'
+    );
   });
 });
