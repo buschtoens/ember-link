855be9db06e1e7aa06af8d129e0a37f750c24be2
Theodor Storm <tstormk@gmail.com>
2020-03-26 16:44:32 +0100

    Fix extra type errors

--- addon/services/link-manager.ts
+++ addon/services/link-manager.ts
@@ -51,7 +51,7 @@ export default class LinkManagerService extends Service {
   /**
    * Creates a `UILink` instance.
    */
-  createUILink(linkParams: LinkParams, uiParams?: UILinkParams): Link | UILink {
+  createUILink(linkParams: LinkParams, uiParams?: UILinkParams): Link {
     return new UILink(this, { ...linkParams, ...uiParams });
   }
 
