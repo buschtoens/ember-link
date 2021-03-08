93bd6aa3d1e7b4c71372614fb9d297f9dd1a9164
Esteban Arango Medina <esteban.arango@envoy.com>
2021-03-08 11:43:19 -0500

    hasAttribute

--- tests/integration/components/link-to-test.ts
+++ tests/integration/components/link-to-test.ts
@@ -17,23 +17,8 @@ module('Component | LinkTo', function (hooks) {
       </LinkTo>
     `);
 
-    assert.dom('[data-test-link]').hasNoAttribute('href');
+    assert.dom('[data-test-link]').hasAttribute('href', `/foo`);
     assert.dom('[data-test-link]').hasNoClass('is-active');
-  });
-
-  test('triggering a transition has no effect', async function (assert) {
-    await render(hbs`
-      <LinkTo @route="foo" data-test-link>
-        Link
-      </LinkTo>
-    `);
-    assert.strictEqual(currentURL(), null);
-
-    const error = await waitForError(() => click('[data-test-link]'));
-    assert.ok(error instanceof Error);
-    // Ember 3.23 and below are throwing "Cannot read property 'hasRoute' of
-    // undefined" here, but we don't care what the exact error is.
-    assert.strictEqual(currentURL(), null);
   });
 
   module('with incomplete models', function () {
