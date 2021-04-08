5e140c3f5ca998fc0c4c81fcc9ac6153dd640678
Jan Buschtöns <jan@buschtoens.me>
2021-04-08 18:01:44 +0200

    tests: fix types issue

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,6 +1,9 @@
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
+// TODO: add types in upstream repo
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import setupSinon from 'ember-sinon-qunit';
 
 import Application from 'dummy/app';
