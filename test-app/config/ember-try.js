93de85a7aa66acb252001a29e586610e7a1e3c7d
gossi <gossi@users.noreply.github.com>
2023-05-30 20:55:13 +0200

    Update ember-try scenarios

---
+++ test-app/config/ember-try.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
+
+module.exports = async function () {
+  return {
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+            'ember-auto-import': '^2.2.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+            'ember-auto-import': '^2.2.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+            'ember-auto-import': '^2.2.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+            'ember-auto-import': '^2.2.0'
+          }
+        }
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release')
+          }
+        }
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta')
+          }
+        }
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary')
+          }
+        }
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false
+          })
+        },
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+            'ember-auto-import': '^2.2.0'
+          },
+          ember: {
+            edition: 'classic'
+          }
+        }
+      },
+      embroiderSafe(),
+      embroiderOptimized()
+    ]
+  };
+};
