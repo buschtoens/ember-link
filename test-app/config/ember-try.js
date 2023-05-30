4595133f8f92546bb1053e0199de9be6e2674b90
gossi <gossi@users.noreply.github.com>
2023-05-30 21:25:44 +0200

    resolve the naming missmatch

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -8,7 +8,7 @@ module.exports = async function () {
     usePnpm: true,
     scenarios: [
       {
-        name: 'ember-lts-3.28',
+        name: 'ember-3.28',
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0'
@@ -16,7 +16,7 @@ module.exports = async function () {
         }
       },
       {
-        name: 'ember-lts-4.4',
+        name: 'ember-4.4',
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0'
@@ -24,7 +24,7 @@ module.exports = async function () {
         }
       },
       {
-        name: 'ember-lts-4.8',
+        name: 'ember-4.8',
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0'
@@ -32,7 +32,7 @@ module.exports = async function () {
         }
       },
       {
-        name: 'ember-lts-4.12',
+        name: 'ember-4.12',
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0'
