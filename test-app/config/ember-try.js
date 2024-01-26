1a84b54033be0ac7abcc94bb91d588043c1d3cd1
gossi <gossi@users.noreply.github.com>
2024-01-26 19:52:37 +0100

    Revert "with types from DT"
    This reverts commit 3fa23f46ad9facd8e2d8638d7227eaa59a6d35f6.

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -3,20 +3,6 @@
 const getChannelURL = require('ember-source-channel-url');
 const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
-const typesFromDefinitelyTyped = {
-  '@types/ember': '^4.0.10',
-  '@types/ember__application': '^4.0.10',
-  '@types/ember__debug': '^4.0.7',
-  '@types/ember__destroyable': '^4.0.4',
-  '@types/ember__helper': '^4.0.5',
-  '@types/ember__modifier': '^4.0.8',
-  '@types/ember__object': '^4.0.11',
-  '@types/ember__owner': '^4.0.8',
-  '@types/ember__routing': '^4.0.19',
-  '@types/ember__runloop': '^4.0.8',
-  '@types/ember__service': '^4.0.8'
-};
-
 module.exports = async function () {
   return {
     usePnpm: true,
@@ -28,8 +14,7 @@ module.exports = async function () {
             '@ember/test-helpers': '^2.9.3',
             'ember-cli': '^4.12.1',
             'ember-source': '~3.28.0',
-            'ember-qunit': '^6.2.0',
-            ...typesFromDefinitelyTyped
+            'ember-qunit': '^6.2.0'
           }
         }
       },
@@ -37,8 +22,7 @@ module.exports = async function () {
         name: 'ember-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
-            ...typesFromDefinitelyTyped
+            'ember-source': '~4.4.0'
           }
         }
       },
@@ -46,8 +30,7 @@ module.exports = async function () {
         name: 'ember-4.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
-            ...typesFromDefinitelyTyped
+            'ember-source': '~4.8.0'
           }
         }
       },
@@ -56,14 +39,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.12.0'
-          }
-        }
-      },
-      {
-        name: 'ember-5.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~5.4.0'
           }
         }
       },
